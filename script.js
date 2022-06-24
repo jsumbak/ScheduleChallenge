42474f43e87d07e64e9fb4eb2ffd8a3f5bae3199
jsumbak <julsumba311@gmail.com>
2022-06-24 16:26:39 -0700

    time blocks added andlocal storage

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
-// console.log("Time shows here");
+
+var saveBtn = $(".saveBtn");
+saveBtn.click(function(event){
+var input = event.target.attributes[0].value
+var inputValue = $("." + input).val()
+    console.log(inputValue)
+    localStorage.setItem(input,inputValue)
+})
+
+$(".nine").val(localStorage.getItem("nine"))
+$(".ten").val(localStorage.getItem("ten"))
+$(".eleven").val(localStorage.getItem("eleven"))
+$(".twelve").val(localStorage.getItem("twelve"))
+$(".one").val(localStorage.getItem("one"))
+$(".two").val(localStorage.getItem("two"))
+$(".three").val(localStorage.getItem("three"))
+$(".four").val(localStorage.getItem("four"))
+$(".five").val(localStorage.getItem("five"))
+
+// momnent js is before and after
